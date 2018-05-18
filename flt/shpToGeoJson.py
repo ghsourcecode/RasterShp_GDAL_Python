262f64a5db95efda1961ba3c88fd64037d4d428d
ghsourcecode <daih1632010@163.com>
2018-05-18 09:55:35 +0800

    修改为传参函数

--- flt/shpToGeoJson.py
+++ flt/shpToGeoJson.py
@@ -3,4 +3,15 @@
 '''
 
 import os
-os.system('python E:/PycharmProject/gdalpython2/gdal223/ogr2ogr.py -f "GeoJSON" E:/PycharmProject/gdalpython2/testdata/out/outputGeoJson.json E:/PycharmProject/gdalpython2/testdata/out/flttoshp.shp')
+def exportShpToGeoJson(shpPath, outGeoJsonPath):
+    currentParentFolderPath = os.path.dirname(os.getcwd())
+    ogr2ogrPath = currentParentFolderPath + '/gdal223/ogr2ogr.py'
+
+    os.system('python' + ' ' + ogr2ogrPath + ' -f "GeoJSON" ' + outGeoJsonPath + ' ' + shpPath)
+    # 输出投影转换后的geojson
+    # os.system('python' + ' ' + ogr2ogrPath + ' -f "GeoJSON" -a_srs epsg:3857 -t_srs epsg:3857 ' + outGeoJsonPath + ' ' + shpPath)
+
+if __name__ == '__main__':
+    shpPath = r'E:\PycharmProject\gdalpython2\testdata\flttoshp.shp'
+    outGeoJsonPath = r'E:\PycharmProject\gdalpython2\testdata\out\shpToGeoJson.json'
+    exportShpToGeoJson(shpPath, outGeoJsonPath)
