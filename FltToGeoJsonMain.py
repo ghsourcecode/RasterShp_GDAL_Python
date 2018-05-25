85fc0f18cd8494ea6e7ba3e116c75110150f37ea
ghsourcecode <daih1632010@163.com>
2018-05-25 09:48:21 +0800

    modify

--- FltToGeoJsonMain.py
+++ FltToGeoJsonMain.py
@@ -9,12 +9,14 @@ import os
 import sys
 import numpy
 import ast
+import shutil
 try:
-    from osgeo import gdal
+    from osgeo import gdal, ogr
 except ImportError:
     import gdal
 import flt.flttoshp as flttoshp
 import flt.shpToGeoJson as shpToGeoJson
+import util.reprojectShp as reprojectShp
 
 classified = [(0, 0, 0.013435),
                 (1, 0.013435, 0.037422),
@@ -28,17 +30,38 @@ classified = [(0, 0, 0.013435),
                 (9, 3.136204, 5.612822)]
 def fltToGeoJson(fltpath, classify, outGeoJsonPath):
     # 下面路径为中间结果路径，程序运行结束后，删除
-    root = os.getcwd()
+    root = sys.path[0]
     tempParent = 'testdata/temp'
-    tempFltToTifPath = tempParent + '/flttotif.tif'
-    classifyTifPath = tempParent + '/classified.tif'
-    maskTifPath = tempParent + '/mask.tif'
+    if not os.path.exists(root + '/' + tempParent):
+        os.makedirs(root + '/' + tempParent)
+    tempFltToTifPath = root + '/' + tempParent + '/flttotif.tif'
+    classifyTifPath = root + '/' + tempParent + '/classified.tif'
+    maskTifPath = root + '/' + tempParent + '/mask.tif'
     tempTifToShpPath = root + '/' + tempParent + '/flttoshp.shp'
     ogr2ogrPath = root + '/gdal223/ogr2ogr.py'
 
     flttoshp.fltToShp(fltpath, tempFltToTifPath, classify, classifyTifPath, maskTifPath, tempTifToShpPath)
-    shpToGeoJson.exportShpToGeoJson(ogr2ogrPath, tempTifToShpPath, outGeoJsonPath)
 
+    # 重投影
+    # 复制一份输出的shp图，并删除原来的，对复制后数据进行投影，并输出到新shp图
+    reprojectShpPath = tempTifToShpPath[: tempTifToShpPath.rfind('.')] + '_reproject.' + tempTifToShpPath[tempTifToShpPath.rfind('.') + 1:]
+    reprojectCalcShpPath = tempTifToShpPath[: tempTifToShpPath.rfind('.')] + '_reproject_calc.' + tempTifToShpPath[tempTifToShpPath.rfind('.') + 1:]
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    tempTifToShpDatasource = None # 将打开的shp datastore置空，为了删除
+    reprojectShp.reproject(tempTifToShpPath, 102025, reprojectShpPath) #epsg:102025为Asia_North_Albers_Equal_Area_Conic等面积投影，用于面积计算
+    if os.access(tempTifToShpPath, os.F_OK):
+        driver.DeleteDataSource(tempTifToShpPath)
+    # 增加计算面积功能
+    flttoshp.calcShpArea(reprojectShpPath, reprojectCalcShpPath)
+
+    reprojectShp.reproject(reprojectCalcShpPath, 4326, tempTifToShpPath)
+    # 处理输出文件目录，保证输出目录存在
+    outGeoJsonPath = outGeoJsonPath.replace('\\', '/')
+    targetFolder = outGeoJsonPath[0 : outGeoJsonPath.rfind('/')]
+    if not os.path.exists(targetFolder):
+        os.makedirs(targetFolder)
+    shpToGeoJson.exportShpToGeoJson(ogr2ogrPath, tempTifToShpPath, outGeoJsonPath)
+    shutil.rmtree(root + '/' + tempParent)
 
 if __name__ == '__main__':
     gdal.AllRegister()
@@ -47,21 +70,21 @@ if __name__ == '__main__':
         sys.exit(0)
 
     # test instance
-    # root = os.getcwd()
-    # fltpath = root + '/testdata/rain_2016.flt'
-    # classify = classified
-    # outGeoJsonPath = root + '/testdata/out/shpToJson.json'
-    # main(fltpath, classify, outGeoJsonPath)
-
-    print('argv 0: ' + argvs[0])
-    print('argv 1: ' + argvs[1].split('=')[1])
-    print('argv 2: ' + argvs[2].split('=')[1])
-    print('argv 3: ' + argvs[3].split('=')[1])
-
-    fltpath = argvs[1].split('=')[1]
-    classifyString = argvs[2].split('=')[1]
-    classifyList = ast.literal_eval(classifyString)
-    classify = numpy.array(classifyList)
-    outGeoJsonPath = argvs[3].split('=')[1]
+    root = os.getcwd()
+    fltpath = root + '/testdata/rain_2016.flt'
+    classify = classified
+    outGeoJsonPath = root + '/testdata/out/shpToJson.json'
     fltToGeoJson(fltpath, classify, outGeoJsonPath)
+
+    # print('argv 0: ' + argvs[0])
+    # print('argv 1: ' + argvs[1].split('=')[1])
+    # print('argv 2: ' + argvs[2].split('=')[1])
+    # print('argv 3: ' + argvs[3].split('=')[1])
+    #
+    # fltpath = argvs[1].split('=')[1]
+    # classifyString = argvs[2].split('=')[1]
+    # classifyList = ast.literal_eval(classifyString)
+    # classify = numpy.array(classifyList)
+    # outGeoJsonPath = argvs[3].split('=')[1]
+    # fltToGeoJson(fltpath, classify, outGeoJsonPath)
 
--- flt/flttoshp.py
+++ flt/flttoshp.py
@@ -1,4 +1,7 @@
+import gdal, ogr
+import os
 from flt import classify as classifier, flttotif, polygonize
+from util import reprojectShp
 
 '''
 分级：
@@ -9,6 +12,16 @@ from flt import classify as classifier, flttotif, polygonize
 0.972118 1.749056 8;1.749056 3.136204 9
 3.136204 5.612822 10
 '''
+classify = [(0, 0, 0.013435),
+                (1, 0.013435, 0.037422),
+                (2, 0.037422, 0.080247),
+                (3, 0.080247, 0.156709),
+                (4, 0.156709, 0.293223),
+                (5, 0.293223, 0.536956),
+                (6, 0.536956, 0.972118),
+                (7, 0.972118, 1.749056),
+                (8, 1.749056, 3.136204),
+                (9, 3.136204, 5.612822)]
 
 def fltToShp(fltPath, fltToTifPath, classify, classifyTifPath, maskTifPath, outShpPath):
     '''
@@ -27,19 +40,72 @@ def fltToShp(fltPath, fltToTifPath, classify, classifyTifPath, maskTifPath, outS
 
     polygonize.polygonize(classifyTifPath, maskTifPath, outShpPath)
 
+def calcShpArea(fromShpPath, toShpPath):
+    '''
+    计算shp面积，增加Shape_Area字段，保存EPSG:3857投影后的面积
+    :param shpPath:
+    :return:
+    '''
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    shpDataSource = driver.Open(fromShpPath)
+    copyDataSource = ogr.GetDriverByName("Memory").CopyDataSource(shpDataSource, "")
+    sourceLayer = copyDataSource.GetLayer(0)
+
+    shpDataSource = None
+    if os.path.exists(fromShpPath):
+        driver.DeleteDataSource(fromShpPath)
+
+    # 增加Shape_Area面积字段
+    areaFieldName = 'Shape_Area'
+    fieldDef = ogr.FieldDefn(areaFieldName, ogr.OFTReal)
+    fieldDef.SetPrecision(2)
+    # fieldDef.SetWidth(10)
+    sourceLayer.CreateField(fieldDef)
+    sourceLayerDef = sourceLayer.GetLayerDefn()
+    areaFieldIndex = sourceLayerDef.GetFieldIndex(areaFieldName)
+
+    # 测试计算面积
+    for feature in sourceLayer:
+        try:
+            gridcodefield = feature.GetField('gridcode')
+            geomref = feature.GetGeometryRef()
+            area = geomref.GetArea()
+            # length = geomref.Length()
+            feature.SetField(areaFieldIndex, area)
+            sourceLayer.SetFeature(feature)
+        except Exception as e:
+            print("异常的类型是:%s"%type(e))
+            print("异常对象的内容是:%s"%e)
+    # 将内存中的datasource复制到磁盘
+    pt_cp = driver.CopyDataSource(copyDataSource, toShpPath)
+    pt_cp.Release()
+
 def fltWithoutPrjToShpTest(fltpath, shppath):
     fltToTifpath = '../testdata/out/flttotif.tif'
     classifyTifPath = '../testdata/out/classified.tif'
     maskTifPath = '../testdata/out/mask.tif'
 
     flttotif.fltWithoutPrjToTiff(fltpath, fltToTifpath)
-    classifier.produceClassifyTif(fltToTifpath, classifyTifPath, None)
+    classifier.produceClassifyTif(fltToTifpath, classify, classifyTifPath)
     classifier.produceMaskTif(fltToTifpath, maskTifPath)
 
     polygonize.polygonize(classifyTifPath, maskTifPath, shppath)
 
+    # 复制一份输出的shp图，并删除原来的，对复制后数据进行投影，输出到复制前的shp图
+    copyShpPath = shppath[ : shppath.rfind('.')] + '_copy.' + shppath[shppath.rfind('.') + 1 : ]
+    copyShpRePrjPath = shppath[ : shppath.rfind('.')] + '_copy_reproject.' + shppath[shppath.rfind('.') + 1 : ]
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    outShpDatasource = driver.Open(shppath)
+    ptsr = driver.CopyDataSource(outShpDatasource, copyShpPath)
+    ptsr.Release()
+    outShpDatasource = None # 释放打开的shp资源
+    if os.access(shppath, os.F_OK):
+        driver.DeleteDataSource(shppath)
+    reprojectShp.reproject(copyShpPath, 3857, copyShpRePrjPath)
 
 if __name__ == '__main__':
     fltpath = '../testdata/rain_2016.flt'
     fltshppath = '../testdata/out/flttoshp.shp'
+
     fltWithoutPrjToShpTest(fltpath, fltshppath)
+    calcShpArea(fltshppath)
